2ca69f5aa974d06046982687cf240649f8e2a30d
SI-Tanbir <si017490@gmail.com>
2024-11-16 09:01:11 +0600

    complete the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './component/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='max-w-[1320px] m-auto'>
     
-    <h2 className='text-4xl'>hello it devolping stage</h2>
-    </>
+   
+   <Header></Header>
+
+    </div>
   )
 }
 
