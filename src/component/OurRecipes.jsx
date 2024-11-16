6c449375ae5388a5c18ac983498b066b0a5b067c
SI-Tanbir <si017490@gmail.com>
2024-11-16 16:16:27 +0600

    fetch data and rendered it

--- src/component/OurRecipes.jsx
+++ src/component/OurRecipes.jsx
@@ -1,5 +1,5 @@
-import { CiClock2 } from "react-icons/ci";
-import { HiFire } from "react-icons/hi";
+
+import Recipes from "./Recipes";
 let OurRecipes=()=>{
 
     return(
@@ -34,49 +34,9 @@ let OurRecipes=()=>{
 
        <div className="">
 
-<div className="card bg-base-100 w-96 shadow-xl">
-  <figure className="px-10 pt-10">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes"
-      className="rounded-xl" />
-  </figure>
-  <div className="card-body ">
-    <h2 className="card-title">Spaghetti Bolognese</h2>
-    <p>Classic Italian pasta dish with savory meat sauce.</p>
 
 
-    <h2 className="card-title">Ingredients: 6</h2>
-
-    <ul className="list-disc	">
-        <li className="ml-8">500g ground beef</li>
-        <li className="ml-8">1 onion, chopped</li>
-        <li className="ml-8">2 cloves garlic, minced</li>
-    </ul>
-
-
-
-
- <div className="flex">
- <span className="text-2xl"><CiClock2 /></span>
- <p>Time</p>
- 
- </div>
-
-
-<div className="flex">
- <span className="text-2xl"><HiFire /></span>
- <p>Calories</p>
- 
- </div>
-
-    <div className="card-actions">
-      <button className="btn bg-[#0BE58A]">Want to Cook</button>
-    </div>
-  </div>
-</div>
-
-
+    <Recipes></Recipes>
 
 
 
@@ -94,6 +54,16 @@ let OurRecipes=()=>{
   <div className="card-body ">
     <h2 className="card-title">Want to Cook :01</h2>
     <hr />
+
+    <div >
+        <ul className="flex gap-5">
+            <li>Name</li>
+            <li>Time</li>
+            <li>Calories</li>
+        </ul>
+
+        
+    </div>
   
 
 
@@ -104,6 +74,8 @@ let OurRecipes=()=>{
 
   </div>
 </div>
+
+
 
 </div>
 
---
+++ src/component/Recipes.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react"
+import fooddata from '../../public/foodData.json'
+import { CiClock2 } from "react-icons/ci";
+import { HiFire } from "react-icons/hi";
+
+
+
+let Recipes=()=>{
+
+    let [count,setCount]=useState([]);
+
+    useEffect(()=>{
+        setCount(fooddata);
+    },[])
+
+console.log(count)
+
+
+    return(
+
+        <div>
+          
+
+        {
+            count.map((output,index) =>(
+
+              
+              
+
+              // starting of recepies card
+              <div key={index} className="card bg-base-100 w-96 shadow-xl">
+
+    
+  <figure className="px-10 pt-10">
+    <img
+      src={output.recipe_image}
+      alt="Shoes"
+      className="rounded-xl" />
+  </figure>
+  <div className="card-body ">
+    <h2 className="card-title">{output.recipe_name}</h2>
+    <p>{output.short_description}</p>
+
+
+    <h2 className="card-title">Ingredients: {output.ingredients.length}</h2>
+
+    <ul className="list-disc	">
+      {
+        output.ingredients.map((ing,index) => (
+          <li key={index} className="ml-8">{ing}</li>
+        ))
+      }
+    </ul>
+
+
+ <div className="flex">
+ <span className="text-2xl"><CiClock2 /></span>
+ <p>Prepared Time: {output.preparing_time}</p>
+ 
+ </div>
+
+
+<div className="flex">
+ <span className="text-2xl"><HiFire /></span>
+ <p>Calories: {output.calories}</p>
+ 
+ </div>
+
+    <div className="card-actions">
+      <button className="btn bg-[#0BE58A]">Want to Cook</button>
+    </div>
+  </div>
+</div>
+                
+            ))
+        }
+
+
+
+
+            
+        
+
+
+
+
+
+
+        </div>
+
+
+
+
+    )
+
+
+}
+
+export default Recipes;
